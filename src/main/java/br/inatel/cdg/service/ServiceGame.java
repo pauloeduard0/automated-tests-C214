83c1b15789e8e94a11b7442d4ba25f1eb5d1699a
pauloeduard0 <pauloedu.rocha@hotmail.com>
2021-09-01 22:31:48 -0300

    Service Game

--- src/main/java/br/inatel/cdg/service/ServiceGame.java
+++ src/main/java/br/inatel/cdg/service/ServiceGame.java
@@ -1,9 +1,35 @@
 package br.inatel.cdg.service;
 
 import br.inatel.cdg.model.Games;
+import br.inatel.cdg.model.Platform;
+import br.inatel.cdg.model.Publisher;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceGame {
+
+    public static List<Games> getListByPlatform(List<Games> games, Platform platform){
+        List<Games> gamesByPlatform = new ArrayList<>();
+
+        games.stream().filter((game ->
+                        game.getPlatform().equals(platform.name()))).
+                forEach(game -> gamesByPlatform.add(game));
+
+        return gamesByPlatform;
+
+    }
+
+
+    public static List<Games> getByPuBlisher(List<Games> games ,
+                                            Publisher publisher){
+        List<Games> gamesByPublisher = new ArrayList<>();
+
+        games.stream().filter((game ->
+                        game.getPublisher().replaceAll("\\s+", "").equals(publisher.name()))).
+                forEach(game -> gamesByPublisher.add(game));
+
+        return gamesByPublisher;
+    }
+
 }
